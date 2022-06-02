161de6ad5eabdbfaf6c734a3693203d91427b05d
kozincov <kozincov1998@gmail.com>
2022-06-02 19:12:47 +0300

    create register-reducer

---
+++ src/reducers/register-reducer.ts
@@ -0,0 +1,28 @@
+import {Dispatch} from "redux";
+import {RegisterArgsType, UserAPI} from "../API/user-api";
+
+const initialState = {
+    isRegisterIn: false
+}
+
+export const registerReducer = (state: InitialStateType = initialState, action: RegisterActionType): InitialStateType => {
+    switch (action.type) {
+        case "register/SET-IS-REGISTER-IN":
+            return {...state, isRegisterIn: action.value}
+        default:
+            return state
+    }
+}
+
+//actions
+export const setIsRegisterInAC = (value: boolean) => ({type: 'register/SET-IS-REGISTER-IN', value} as const)
+//thunks
+export const registerTC = (data: RegisterArgsType) => (dispatch: Dispatch) => {
+    UserAPI.register(data)
+        .then((res) => {
+            dispatch(setIsRegisterInAC(true))
+        })
+}
+//types
+type InitialStateType = typeof initialState
+export type RegisterActionType = ReturnType<typeof setIsRegisterInAC>
