310b0a0e7ddd656f694e0e8c447c2598a59a6275
kozincov <kozincov1998@gmail.com>
2022-06-02 19:59:58 +0300

    added app-reducer in store

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -2,10 +2,12 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from 'redux-thunk'
 import {authReducer} from "./auth-reducer";
 import {registerReducer} from "./register-reducer";
+import {appReducer} from "./app-reducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    register: registerReducer
+    register: registerReducer,
+    app: appReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
