b0c23f4314e010bb10f1518b5d4abd2b87197fbb
kozincov <kozincov1998@gmail.com>
2022-06-02 19:20:22 +0300

    update sign up page

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,9 +1,11 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from 'redux-thunk'
 import {authReducer} from "./auth-reducer";
+import {registerReducer} from "./register-reducer";
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    register: registerReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
