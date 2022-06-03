35d8892a7f5e414b63206f4e657d1d4ad0c78f64
kozincov <kozincov1998@gmail.com>
2022-06-03 15:05:23 +0300

    create initialize-reducer

---
+++ src/reducers/initialize-reducer.ts
@@ -0,0 +1,36 @@
+import {Dispatch} from "redux";
+import {UserAPI} from "../API/user-api";
+import {handleServerNetworkError} from "../error-utils/error";
+import {setIsRegisterInAC} from "./register-reducer";
+
+const initialState = {
+    isInitializeIn: false
+}
+
+export const initializeReducer = (state: InitialStateType = initialState, action: RegisterActionType): InitialStateType => {
+    switch (action.type) {
+        case "initialize/SET-INITIALIZE-IN":
+            return {...state, isInitializeIn: action.isInitializeIn}
+        default:
+            return state
+    }
+}
+
+//actions
+export const setInitializeAC = (isInitializeIn: boolean) => {
+    return {type: "initialize/SET-INITIALIZE-IN", isInitializeIn} as const
+}
+
+//thunks
+export const InitializeTC = () => (dispatch: Dispatch) => {
+    UserAPI.me()
+        .then((res) => {
+            dispatch(setIsRegisterInAC(true))
+        })
+        .catch(error => {
+            handleServerNetworkError(error.response.data.error, dispatch)
+        })
+}
+//types
+type InitialStateType = typeof initialState
+export type RegisterActionType = ReturnType<typeof setInitializeAC>
