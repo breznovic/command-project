9ee9078a28dc7fa0123cf757bf999c812c7f4b0a
kozincov <kozincov1998@gmail.com>
2022-06-03 15:04:51 +0300

    added initialize-reducer in store

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk'
 import {authReducer} from "./auth-reducer";
 import {registerReducer} from "./register-reducer";
 import {appReducer} from "./app-reducer";
+import {initializeReducer} from "./initialize-reducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     register: registerReducer,
-    app: appReducer
+    app: appReducer,
+    initialize: initializeReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
