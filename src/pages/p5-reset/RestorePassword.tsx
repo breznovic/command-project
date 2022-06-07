00f7fe247ced4a1d5949ba78e42a0e72d2afa71b
breznovic <slymarkel@gmail.com>
2022-06-07 19:55:57 +0300

    New Password and invalid restore 2

--- src/pages/p5-reset/RestorePassword.tsx
+++ src/pages/p5-reset/RestorePassword.tsx
@@ -6,7 +6,7 @@ import Input from "../../common/input/Input";
 import Checkbox from "../../common/checkbox/Checkbox";
 import Button from "../../common/button/Button";
 import {useAppDispatch} from "../../reducers/store";
-import { useParams } from "react-router-dom"
+import {useParams} from "react-router-dom"
 
 type FormikErrorType = {
     email?: string
@@ -14,23 +14,21 @@ type FormikErrorType = {
 
 const RestorePassword = () => {
 
-
-
     const dispatch = useAppDispatch()
 
     const formik = useFormik({
         initialValues: {
             password: '',
             resetPasswordToken: '',
-            // email: ''
+            email: ''
         },
         validate: (values) => {
             const errors: FormikErrorType = {};
-            // if (!values.email) {
-            //     errors.email = "email must not be a null"
-            // } else if (values.email.length < 4) {
-            //     errors.email = 'To small email';
-            // }
+            if (!values.email) {
+                errors.email = "email must not be a null"
+            } else if (values.email.length < 4) {
+                errors.email = 'To small email';
+            }
             return errors;
 
         },
@@ -43,26 +41,22 @@ const RestorePassword = () => {
     return <form onSubmit={formik.handleSubmit}>
         <div>
 
-            <h2>New Password</h2>
-
-
-            <Input placeholder={'restore password'}
+            <h2>Restore your password</h2>
+            <Input placeholder={'Enter your Email'}
                 // newPassword
-                   {...formik.getFieldProps("password")}
+                   {...formik.getFieldProps("email")}
 
             />
         </div>
-        {/*{formik.touched.email &&*/}
-        {/*    formik.errors.email &&*/}
-        {/*    <div style={{color: 'red'}}>{formik.errors.email}</div>}*/}
-        <p>Enter your Email.</p>
-        <p> An to your email will be sent letter to reset your password</p>
+        {formik.touched.email &&
+            formik.errors.email &&
+            <div style={{color: 'red'}}>{formik.errors.email}</div>}
+        <p>Enter your Email</p>
+        <p>New password will be sent on your Email</p>
         <Button>
-            Send Letter
+            Send
         </Button>
-
     </form>
-
 };
 
 export default RestorePassword;
